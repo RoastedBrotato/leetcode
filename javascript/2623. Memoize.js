0ae187936852dc33ea417fcbd4d95f34f1cf27db
Waleed <waleedajaz1996@gmail.com>
2023-10-03 10:50:36 +0300

    Create 2623. Memoize.js

---
+++ javascript/2623. Memoize.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        const key = String(args);
+        if(key in cache){
+        return cache[key];
+        }
+
+        const result = fn(...args);
+        cache[key] = result;
+        return result;
+    }
+}

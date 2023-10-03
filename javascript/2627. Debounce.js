ac35fdde1401caaab7683f59e99a3dab997d241b
Waleed <waleedajaz1996@gmail.com>
2023-10-03 10:53:27 +0300

    Create 2627. Debounce.js

---
+++ javascript/2627. Debounce.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), t);
+    }
+};

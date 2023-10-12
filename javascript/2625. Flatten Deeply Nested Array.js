8e0b71eabee28dff0073827d3e00d2948f47fc56
Waleed <waleedajaz1996@gmail.com>
2023-10-12 08:28:43 +0300

    Create 2625. Flatten Deeply Nested Array.js

---
+++ javascript/2625. Flatten Deeply Nested Array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    if(n == 0){
+        return arr;
+    }
+
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            result.push(...flat(arr[i], n-1));
+        }
+        else{
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+};
